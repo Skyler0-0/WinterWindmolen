8bae224c53e2fd5f81deff95d701ee5e18de9a19
Skyler <39634@ma-web.nl>
2025-09-01 23:51:03 +0200

    first

---
+++ Assets/Script/Key.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class Key : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+}
